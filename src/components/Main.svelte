ce56acc6c34e2c7002e63e07b88d3f8948211689
James McArthur <77163783+jamezmca@users.noreply.github.com>
2023-08-21 12:46:55 +0100

    nice

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -12,6 +12,8 @@
             inputColor: darkTheme ? " bg-slate-950" : " bg-white",
             accentColor: darkTheme ? " text-blue-400" : " text-blue-500",
             fadedColor: darkTheme ? " text-slate-200 " : " text-slate-500",
+            blueText: darkTheme ? " text-blue-400" : " text-blue-500",
+            amberText: darkTheme ? " text-amber-400" : " text-amber-500",
         };
     }
 
@@ -30,19 +32,27 @@
 </script>
 
 <main
-    class="flex-1 p-4 max-w-[1000px] mx-auto w-full flex flex-col min-h-[400px] sm:min-h-[500px] "
+    class="flex-1 p-4 max-w-[1000px] mx-auto w-full flex flex-col min-h-[400px] sm:min-h-[500px]"
 >
     {#if !$store.name}
         <div
-            class="flex flex-col justify-center items-center flex-1 gap-8 max-w-prose w-full mx-auto pt-14 sm:pt-20"
+            class="flex flex-col justify-center items-center flex-1 gap-6 max-w-prose w-full mx-auto pt-14 pb-7 sm:pb-10 sm:pt-20"
         >
-            <div class="flex flex-col gap-4 text-center">
+            <div class="flex flex-col gap-6 text-center">
                 <h1 class="text-3xl sm:text-4xl md:text-5xl font-semibold">
-                    Welcome to the Webdev <span class={" " + styles.accentColor}
-                        >Roadmap</span
-                    >
+                    Welcome to the Webdev
+                    <span class={" " + styles.accentColor}>Roadmap</span>
                 </h1>
-                <p class={"italic " + styles.fadedColor}>- by Smoljames</p>
+                <h4 class="">
+                    <span class=" font-semibold">Level up</span> your
+                    programming skills from
+                    <span class={"font-semibold " + styles.blueText}>zero</span>
+                    to
+                    <span class={"font-semibold " + styles.amberText}>hero</span
+                    >! <br /><span class="">No experience required.</span>
+                </h4>
+
+                <!-- <p class={"italic " + styles.fadedColor}>- by Smoljames</p> -->
             </div>
             <div class="flex p-0.5 w-full relative">
                 <div
@@ -66,12 +76,16 @@
                     <button
                         on:click={handleSaveName}
                         class={" py-2 rounded-md  hover:opacity-60 px-3 duration-200   " +
-                            styles.buttonBackground}
+                            styles.buttonBackground + ' ' + (name ? ' text-cyan-400 scale-[105%] sm:scale-110' : ' ')}
                     >
                         <i class="fa-solid fa-arrow-right" />
                     </button>
                 </div>
             </div>
+            <!-- <p class="text-sm opacity-80">
+                Input your name and hit <span class="font-semibold">Enter</span>
+                to begin
+            </p> -->
         </div>
     {:else}
         <Roadmap />
